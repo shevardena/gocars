b1b3578c8eeeef6c74f9114ef6aa90ff70719f68
IrakliShevardenidze <i.shevardenidze@optimogroup.io>
2025-09-16 15:52:18 +0400

    installed spatie sluggable package and added slugable trait to carmake model

--- app/Models/CarMake.php
+++ app/Models/CarMake.php
@@ -3,9 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class CarMake extends Model
 {
+
+    use HasSlug;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
     protected $fillable = [
         'id',
         'name',
