5a8f20fd3a122a3cb99db9669deac718e589eef1
IrakliShevardenidze <i.shevardenidze@optimogroup.io>
2025-09-23 10:49:02 +0400

    added roles choose to backend users

--- app/Filament/Resources/BackendUsers/Schemas/BackendUserForm.php
+++ app/Filament/Resources/BackendUsers/Schemas/BackendUserForm.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\BackendUsers\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Illuminate\Support\Facades\Hash;
 
 class BackendUserForm
@@ -27,8 +30,14 @@ class BackendUserForm
                     ->dehydrateStateUsing(fn (string $state): string => Hash::make($state))
                     ->dehydrated(fn (?string $state): bool => filled($state))
                     ->required(fn (string $operation): bool => $operation === 'create'),
+                Select::make('role_id')
+                    ->label('Role')
+                    ->required()
+                    ->multiple()
+                    ->relationship(name: 'roles', titleAttribute: 'name')
+                    ->searchable(),
+                TextColumn::make('false'),
                 Toggle::make('super_admin'),
-                DateTimePicker::make('last_login'),
             ]);
     }
 }
